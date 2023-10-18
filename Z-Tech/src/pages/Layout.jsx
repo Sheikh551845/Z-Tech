4add71def7cb1dced2a86a66b7a51c6634923c36
sheikh551845 <sheikh551845@gmail.com>
2023-10-18 11:16:04 +0600

    Footer added

--- Z-Tech/src/pages/Layout.jsx
+++ Z-Tech/src/pages/Layout.jsx
@@ -4,13 +4,14 @@ import { Outlet } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from '../Components/Footer'
 
 export default function Layout() {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
-
+      <Footer></Footer>
       <ToastContainer></ToastContainer>
     </div>
   )
