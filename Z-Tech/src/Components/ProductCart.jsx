c79a107c232dec06ba0a64d3ef295cc26a5d9d76
sheikh551845 <sheikh551845@gmail.com>
2023-10-20 11:25:21 +0600

    All product page added

---
+++ Z-Tech/src/Components/ProductCart.jsx
@@ -0,0 +1,70 @@
+import React, { useContext } from 'react'
+import { AuthContext } from './AuthProvider';
+import { Link } from 'react-router-dom';
+
+export default function ProductCart(product) {
+
+    const{setCartData,CartData}=useContext(AuthContext)
+
+
+    const {productName,price, imageUrl,_id,brandName,productType,description,rating}=product.product;
+
+
+  return (
+    <div className="max-w-fit mx-auto ">
+    <div className="relative flex w-[25rem] h-fit p-2 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+    <div className="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none">
+    <img
+    src={imageUrl}
+    alt=""
+    className='h-52 w-full'
+    />
+    </div>
+    <div className="p-6">
+    <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-indigo-700 antialiased">
+    {brandName}
+    </h4>
+    <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-orange-700 antialiased">
+    {productName}
+    </h4>
+    <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-black antialiased">
+    Product Type : <span className="font-normal">{productType}</span>
+    </h4>
+    <p className="mt-3 block font-sans text-xl font-semibold leading-relaxed text-gray-700 antialiased">
+    Price : <span className="font-normal">{price}</span>
+    </p>
+    <p className="mt-3 block font-sans text-xl font-semibold leading-relaxed text-gray-700 antialiased">
+    Rating : <span className="font-normal">{rating}</span>
+    </p>
+
+    <p className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
+    {description}
+    </p>
+
+    </div>
+    <div className="p-6 pt-0 flex justify-between">
+    <Link to={`/ProductDetails/${_id}`}> 
+  <button
+    className="select-none rounded-lg bg-black py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-indigo-400 transition-all hover:shadow-lg hover:shadow-indigo-700 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    type="button"
+    data-ripple-light="true"
+  >
+    Show Details
+  </button>
+  </Link>
+    
+  <button
+    className="select-none rounded-lg bg-black py- px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-indigo-400 transition-all hover:shadow-lg hover:shadow-indigo-700 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    type="button"
+    data-ripple-light="true"
+    // onClick={()=>handleDelete(_id)}
+  >
+   Update
+  </button>
+  
+   
+    </div>
+    </div>
+    </div>
+  )
+}
--- Z-Tech/src/pages/AllProducts.jsx
+++ Z-Tech/src/pages/AllProducts.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../Components/AuthProvider';
+import ProductCart from '../Components/ProductCart';
 
 export default function AllProducts() {
+
+
+  const {data}=useContext(AuthContext);
   return (
-    <div>
-      This is all products page 
-    </div>
+    <div className="w-screen my-24 ">
+      <div className="">
+             
+      <p className="lg:text-4xl text-3xl font-extrabold leading-9 text-indigo-700 w-fit mx-auto my-5">All products</p> 
+
+
+        
+        
+<div className=" flex flex-wrap gap-4 max-w-fit mx-auto   ">
+{
+data?.map((product, index) => (
+<ProductCart key={index} product={product}></ProductCart>
+))
+}
+</div>
+
+
+      </div>
+
+      
+      </div>
+
+        
+     
+   
   )
 }
